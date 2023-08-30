d0ab69c163cb23a1a95bece17fdd4e34909f5096
Jacob Frank <frankobjank@gmail.com>
2023-08-29 20:50:56 -0400

    another change

--- test_branch_merging.py
+++ test_branch_merging.py
@@ -1,11 +1,11 @@
 # merging to main branch
 print("Hello")
+print("This is version 1")
 list5 = [13, 14, 15] # list on main
 list7 = [16, 17, 18] # list on test
 def functino():
     print("Why")
     
-print("This is version 1")
 
 list2 = [4, 5, 6]
 
